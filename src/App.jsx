2e4f78167cd8385cf6499e74098a5495259f17b3
OjaswiPande <ojaswipande@gmail.com>
2025-07-02 21:25:58 +0530

    Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
+import Home from './pages/Home'
 import RegisterForm from './pages/RegisterForm'
 import LoginForm from './pages/LoginForm'
 import AdminDashboard from './pages/AdminDashboard'
@@ -13,39 +14,39 @@ import VendorListDisplay from './pages/VendorListDisplay'; // ✅ Import added
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<RegisterForm />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/vendorlist" element={<VendorListDisplay />} /> {/* ✅ Route added */}
-      </Routes>
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      
-      <Routes>
-        
-        
-        <Route path="/vendor-dashboard"/>
+  <Routes>
+    <Route path="/" element={<RegisterForm />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/login" element={<LoginForm />} />
+    <Route path="/vendorlist" element={<VendorListDisplay />} />
 
-        <Route path="/admin-dashboard" element = {
-          <ProtectedRoute allowedRoles={["ADMIN"]}>
-            <AdminDashboard/>
-          </ProtectedRoute>
-        }/>
+    <Route
+      path="/admin-dashboard"
+      element={
+        <ProtectedRoute allowedRoles={["ADMIN"]}>
+          <AdminDashboard />
+        </ProtectedRoute>
+      }
+    />
 
-        <Route path="*" element={<PageNotFound/>}/>
+    {/* 404 fallback should be last */}
+    <Route path="*" element={<PageNotFound />} />
+  </Routes>
 
-      </Routes>
-    </Router>
+  <ToastContainer
+    position="top-right"
+    autoClose={3000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+  />
+</Router>
+
   );
 }
 
